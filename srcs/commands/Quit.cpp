0afde4bdba75893da37cafa07a47d4d7d1b98f80
soufiixarif <arif.soufiix@gmail.com>
2025-07-04 17:43:57 +0100

    quit done + fixes in part

--- srcs/commands/Quit.cpp
+++ srcs/commands/Quit.cpp
@@ -1,19 +1,28 @@
 #include "../../includes/Server.hpp"
 
 int Server::handelQuit(Client* client, const std::vector<std::string>& params){
-    (void)params;
     std::vector<Channel *>::iterator it;
-    for (it = this->chanPool.begin(); it != this->chanPool.end(); it++){
+    for (it = this->chanPool.begin(); it != this->chanPool.end();){
         (*it)->deleteFromContainer(client, (*it)->getMembers());
         (*it)->deleteFromContainer(client, (*it)->getOperators());
+        std::string message = ":" + client->getNickName() + "!" + client->getUserName() + "@localhost PART " + (*it)->getName() + " :" + params[0] + "\r\n";
+        broadcastInChannel((*it)->getMembers(), message);
         if ((*it)->getOperators().empty()){
-            if ((*it)->getMembers().size() > 1)
+            if ((*it)->getMembers().size() >= 1){
                 (*it)->addToContainer((*it)->getMembers()[0], (*it)->getOperators());
+                std::string announce = ":" + client->getNickName() + "!" + "localhost MODE "
+                + (*it)->getName() + " +o " + (*it)->getMembers()[0]->getNickName();
+             broadcastInChannel((*it)->getMembers(), announce);
+             it++;
+            }
             else{
-                delete (*it);
-                this->chanPool.erase(it);
+                delete(*it);
+                it = this->chanPool.erase(it);
+                continue;
             }
         }
+        else
+            it++;
     }
     return 0;
 }
